ef5f4f199de04ec7489fd75f4bb4e74c8fbebf95
claudiainbytes <claudiaeforero@hotmail.com>
2023-05-31 23:40:45 -0500

    Slick JS on Carousel

--- public/js/app.js
+++ public/js/app.js
@@ -28,5 +28,16 @@ var fxToggleTheme = function() {
 
 btnToggleTheme.addEventListener('click', fxToggleTheme);
 
+// Slick Carousel Fade
 
+$('#carousel').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    autoplay: true,
+    autoplaySpeed: 5000,
+    arrows: false,
+  });
   
